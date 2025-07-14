473938dac466fb27f9bcc43e9021d064e78fdd98
RonaldMullo <electroascensores64@gmail.com>
2025-07-13 22:43:01 -0500

    Update Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    static void DisWel() 
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string ProNam() 
+    {
+        Console.Write("Please enter your name: ");
+        string nam = Console.ReadLine();
+        return nam;
+    }
+
+    static int ProNum() 
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquNum(int num) 
+    {
+        int sqr = num * num;
+        return sqr;
+    }
+
+    static void DisRes(string nam, int sqr) 
+    {
+        Console.WriteLine($"{nam}, the square of your number is {sqr}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisWel();
+        string nam = ProNam();
+        int num = ProNum();
+        int sqr = SquNum(num);
+        DisRes(nam, sqr);
     }
 }
